fb4c94731b0b4382510bedf56116eefc2f77e4d2
mitrandir-pl <leshkopavel@gmail.com>
2021-11-02 18:16:23 +0300

    lab3 added

---
+++ lab3/test_laba_3.py
@@ -0,0 +1,47 @@
+import unittest
+from laba_3 import gluing, calculation_method, calculation_tabular_method, tabular_method
+
+
+class TestLaba(unittest.TestCase):
+
+    def test_gluing(self):
+        # SDNF
+        self.assertEqual(gluing([["!a", "b", "c"], ["a", "!b", "!c"], ["a", "!b", "c"], ["a", "b", "c"]]), [['b', 'c'], ['a', '!b'], ['a', 'c']])
+        self.assertEqual(gluing([["!a", "!b", "!c"], ["a", "!b", "!c"], ["a", "b", "!c"], ["a", "b", "c"]]), [['!b', '!c'], ['a', '!c'], ['a', 'b']])
+        # # SKNF
+        self.assertEqual(gluing([["a", "b", "c"], ["a", "b", "!c"], ["a", "!b", "c"], ["!a", "!b", "c"]]), [['a', 'b'], ['a', 'c'], ['!b', 'c']])
+        self.assertEqual(gluing([["a", "b", "!c"], ["a", "!b", "c"], ["a", "!b", "!c"], ["!a", "b", "!c"]]), [['a', '!c'], ['b', '!c'], ['a', '!b']])
+
+    def test_calculation_method(self):
+        # SDNF
+        self.assertEqual(calculation_method([['b', 'c'], ['a', '!b'], ['a', 'c']], "dnf"), [["b", "c"], ["a", "!b"]])
+        self.assertEqual(calculation_method([['!b', '!c'], ['a', '!c'], ['a', 'b']], "dnf"), [['!b', '!c'], ['a', 'b']])
+        # # SKNF
+        self.assertEqual(calculation_method([['a', 'b'], ['a', 'c'], ['!b', 'c']], "knf"), [["a", "b"], ["!b", "c"]])
+        self.assertEqual(calculation_method([['a', '!c'], ['b', '!c'], ['a', '!b']], "knf"), [['b', '!c'], ['a', '!b']])
+
+    def test_calculation_tabular_method(self):
+        # SDNF
+        self.assertEqual(calculation_tabular_method(
+            [["b", "c"], ["a", "!b"], ["a", "c"]],
+            [["!a", "b", "c"], ["a", "!b", "!c"], ["a", "!b", "c"], ["a", "b", "c"]],
+            "dnf"),
+            [["b", "c"], ["a", "!b"]])
+        # SKNF
+        self.assertEqual(calculation_tabular_method([['a', 'b'], ['a', 'c'], ['!b', 'c']],
+            [["a", "b", "c"], ["a", "b", "!c"], ["a", "!b", "c"], ["!a", "!b", "c"]],
+            "knf"),
+            [["a", "b"], ["!b", "c"]])
+
+    def test_tabular_method(self):
+        # SDNF
+        self.assertEqual(tabular_method(
+            [["!a", "b", "c"], ["a", "!b", "!c"], ["a", "!b", "c"], ["a", "b", "c"]],),
+            [["b", "c"], ["a", "!b"]])
+        # # SKNF
+        self.assertEqual(tabular_method(
+            [["a", "b", "c"], ["a", "b", "!c"], ["a", "!b", "c"], ["!a", "!b", "c"]]),
+            [["!b", "c"], ["a", "b"]])
+
+if __name__ == "__main__":
+    unittest.main()
